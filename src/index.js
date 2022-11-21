41961b3774a9273fd98ee3ef8df487c6f462860f
Ahmed Shehab <rs@MacBook-Pro.lan>
2022-11-22 00:55:21 +0400

    remove purge as it is built in jit

--- src/index.js
+++ src/index.js
@@ -15,26 +15,11 @@ module.exports = function (api) {
     api.compatibleWith('@quasar/app', '^1.0.0 || ^2.0.0 || ^3.0.0-beta')
     // api.compatibleWith('postcss', '^8.1.0') // using compat build for now
 
-    const purgecss = require('@fullhuman/postcss-purgecss')({
-
-        // Specify the paths to all of the template files in your project
-        content: [
-            api.resolve.src('**/*.html'),
-            api.resolve.src('**/*.vue'),
-        ],
-
-        // Include any special characters you're using in this regular expression
-        defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
-    })
-
     api.chainWebpack((cfg, {isClient, isServer}, api) => {
         const plugins = [
             require('tailwindcss')(api.resolve.src('extensions/tailwindcss/tailwind.config.js')),
             require('autoprefixer'),
         ];
-        if (api.ctx.prod) {
-            plugins.push(purgecss);
-        }
 
         cfg.module
             .rule('tailwind')
